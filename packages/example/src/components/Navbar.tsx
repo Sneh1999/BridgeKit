59659b990784ef9ab3bdc1bfae8878012186e961
Sneh Koul <snehkoul1999@gmail.com>
2023-08-20 05:36:54 -0400

    add simple logo

--- packages/example/src/components/Navbar.tsx
+++ packages/example/src/components/Navbar.tsx
@@ -4,8 +4,10 @@ import React from "react";
 
 export const Navbar: React.FC = () => {
   return (
-    <div className="flex items-center justify-between px-6 py-2">
-      <Link href="/">Home</Link>
+    <div className="flex items-center justify-between border-b px-6 py-2">
+      <Link href="/" className="text-3xl font-extrabold">
+        🚀 BridgeKit
+      </Link>
       <ConnectButton />
     </div>
   );
